382f65e31bea424f2321802d4b7ac62c67a5af02
Syuto01 <49546752+Syuto01@users.noreply.github.com>
2019-06-28 09:50:23 +0900

    Delete createDestination.js

--- WebContent/js/createDestination.js
+++
@@ -1,9 +0,0 @@
-function goCreateDestinationAction(){
-	document.getElementById("backFlg").value="1";
-	var id = document.getElementById("createDestinationForm").action="CreateDestinationAction";
-}
-
-function goCreateDestinationCompleteAction(){
-	document.getElementById("createDestinationForm").action="CreateDestinationCompleteAction";
-}
-
