9c78615aae76779c794ff01912253e7260a7b699
Syuto01 <49546752+Syuto01@users.noreply.github.com>
2019-06-28 09:49:56 +0900

    Delete resetPassword.js

--- WebContent/js/resetPassword.js
+++
@@ -1,7 +0,0 @@
-function goResetPasswordAction(){
-	document.getElementById("backFlag").value="1";
-	document.getElementById("resetPasswordForm").action="ResetPasswordAction";
-}
-function goResetPasswordCompleteAction(){
-	document.getElementById("resetPasswordForm").action="ResetPasswordCompleteAction";
-}
